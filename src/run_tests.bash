1f8f6b0b2c44daaf1641018809eb6e991131cbde
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2020-05-29 12:13:36 -0400

    issue soothing message for failure test case (20)

--- src/run_tests.bash
+++ src/run_tests.bash
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 num_tests=20
+start_test=1
 
 td=`rospack find tagslam_test`
-for i in $(seq 1 $num_tests);
+for i in $(seq $start_test $num_tests);
 do
     echo -n 'running test ' $i ' ... '
     roslaunch tagslam_test test.launch test:=$i > /dev/null
     $td/src/compare_poses.py $td/tests/test_$i/reference.bag ~/.ros/out.bag
     if [ $? -eq 0 ]; then
        echo 'succeeded!'
+       if [ $i -eq 20 ]; then
+	    echo '(this test is a failure test, the scary messages are ok)'
+       fi
     else
        echo 'FAILED!'
     fi
