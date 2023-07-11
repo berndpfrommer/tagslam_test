da8d46465a1d2980f3e44f7662191e0061b415ee
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2023-07-11 08:18:48 -0400

    added test_24

--- src/run_tests.bash
+++ src/run_tests.bash
@@ -7,8 +7,8 @@ td=`rospack find tagslam_test`
 for i in $(seq $start_test $num_tests);
 do
     echo -n 'running test ' $i ' ... '
-    roslaunch tagslam_test test.launch test:=$i > /dev/null
-    $td/src/compare_poses.py $td/tests/test_$i/reference.bag ~/.ros/out.bag
+    roslaunch tagslam_test test.launch test:=$i > /dev/null &&
+    python3 $td/src/compare_poses.py $td/tests/test_$i/reference.bag ~/.ros/out.bag
     if [ $? -eq 0 ]; then
        echo 'succeeded!'
        if [ $i -eq 20 ]; then
---
+++ tests/test_24/README.md
@@ -0,0 +1,15 @@
+# Example 24
+
+- Location: Unknown
+- Cameras:
+   . single camera (brand unknown) 2880 x 1860 
+- Calibration: radtan
+- Source bag: abc.bag
+- detector: UMich
+- frames: 3001
+- Remark: Tests the "staggered_board" body type. Uses the UMich
+  Apriltag detector which finds many small tags at very shallow
+  angles. This requires a lot of tags to be dropped to avoid poor
+  initialization.
+
+
